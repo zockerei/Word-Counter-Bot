554f8c288068af048ccae99c58ecda69a0eabf65
zockerei <2paulengelhardt@hitler.rocks>
2023-12-02 13:33:06 +0100

    Added unittesting for create_tables get_count and add_words. Changed name for unittest bot. Renamed create_table and add_guild_members. Foreign Keys now in user_has_word.

--- unit_tests.py
+++ unit_tests.py
@@ -1,11 +1,10 @@
 import logging.config
-import sqlite3
 import unittest
 import yaml
 import sql
 
 
-class TestDatabaseModule(unittest.TestCase):
+class TestSqlModule(unittest.TestCase):
     _logger = None
 
     @classmethod
@@ -15,15 +14,136 @@ class TestDatabaseModule(unittest.TestCase):
             logging_config = yaml.safe_load(config_file.read())
 
         logging.config.dictConfig(logging_config)
-        cls._logger = logging.getLogger('unittest')
+        cls._logger = logging.getLogger('bot.unittest')
+        cls._logger.debug('Logging config complete')
 
     def setUp(self):
-        # Create a connection to database
-        self._connection = sqlite3.connect(':memory:')
-        self._cursor = self._connection.cursor()
+        # Create sql_statements variable
+        self.sql_statements = sql.SqlStatements()
 
-        self._logger.debug('help')
+        # Create tables
+        self.sql_statements.create_tables()
+
+    def testCreateTables(self):
+        self._logger.debug("Testing creation of 'user' table")
+        self.sql_statements.create_tables()
+
+        # Check if 'user' table exists
+        self._logger.debug('User table creation test')
+        self.sql_statements.cursor.execute(
+            """select name from sqlite_master
+            where type='table' and name='user'"""
+        )
+        table_exists = self.sql_statements.cursor.fetchone() is not None
+        self.assertTrue(table_exists, "The 'user' table does not exist")
+        self._logger.info('User table created successfully')
+
+        # Check if 'user' table has expected columns
+        self._logger.debug('User table columns test')
+        self.sql_statements.cursor.execute("pragma table_info(user)")
+        columns = [row[1] for row in self.sql_statements.cursor.fetchall()]
+        expected_columns = ['id']
+        self.assertCountEqual(columns, expected_columns, "'user' table does not have an 'id' column")
+        self._logger.info("'user' table columns validated")
+
+        # Check if 'word' table exists
+        self._logger.debug('Word table creation test')
+        self.sql_statements.cursor.execute(
+            """select name from sqlite_master
+            where type='table' and name='word'"""
+        )
+        table_exists = self.sql_statements.cursor.fetchone() is not None
+        self.assertTrue(table_exists, "The 'word' table does not exist")
+        self._logger.info('Word table created successfully')
+
+        # Check if 'word' table has expected columns
+        self._logger.debug('Word table columns test')
+        self.sql_statements.cursor.execute("pragma table_info(word)")
+        columns = [row[1] for row in self.sql_statements.cursor.fetchall()]
+        expected_columns = ['name']
+        self.assertCountEqual(columns, expected_columns, "'word' table does not have a 'name' column")
+        self._logger.info("'word' table columns validated")
+
+        # Check if 'user_has_word' table exists
+        self._logger.debug('User_has_word table creation test')
+        self.sql_statements.cursor.execute(
+            """select name from sqlite_master
+            where type='table' and name='user_has_word'"""
+        )
+        table_exists = self.sql_statements.cursor.fetchone() is not None
+        self.assertTrue(table_exists, "The 'user_has_word' table does not exist")
+        self._logger.info('User_has_word table created successfully')
+
+        # Check if 'user_has_word' table has expected columns
+        self._logger.debug('User_has_word table columns test')
+        self.sql_statements.cursor.execute("pragma table_info(user_has_word)")
+        columns = [row[1] for row in self.sql_statements.cursor.fetchall()]
+        expected_columns = ['user_id', 'word_name', 'count']
+        self.assertCountEqual(columns, expected_columns, "'user_has_word' table does not have the expected columns")
+        self._logger.info("'user_has_word' table columns validated")
+        self._logger.debug("Finished createTables test")
+
+    def testAddWords(self):
+        self._logger.debug('Testing add_words method')
+
+        words_to_add = ['word1', 'word2', 'word3']
+
+        self._logger.debug('Calling add_words method')
+        self.sql_statements.add_words(words_to_add)
+
+        # Verify that the words are added to the database
+        self._logger.debug('Verifying words added to database')
+        self.sql_statements.cursor.execute(
+            """select * from word
+            where name in (:word1, :word2, :word3)""",
+            {'word1': words_to_add[0], 'word2': words_to_add[1], 'word3': words_to_add[2]}
+        )
+
+        added_words = [row[0] for row in self.sql_statements.cursor.fetchall()]
+
+        self.assertCountEqual(added_words, words_to_add, 'Not all words were added to the database')
+        self._logger.info('add_words method tested successfully')
+
+    def testAddUserIds(self):
+        self._logger.debug('Testing add_guild_members method')
+
+        # Define the guild members to add
+        guild_members = {372045873095639040, 123456789012345678, 987654321098765432}
+
+        # Call add_guild_members method
+        self._logger.debug('Calling add_guild_members method')
+        self.sql_statements.add_user_ids(guild_members)
+
+        # Verify that the users were added to the database
+        added_users = self.sql_statements.get_all_users()
+
+        self.assertSetEqual(set(added_users), guild_members, 'Not all users were added to the database')
+        self._logger.info('add_guild_members method tested successfully')
+
+    def testGetCount(self):
+        self._logger.debug('Testing get_count method')
+
+        # Add a user and a word to the database
+        user_id = 123456789012345678
+        word = 'example_word'
+        self.sql_statements.add_user_ids(user_id)
+        self.sql_statements.add_words(word)
+
+        # Add a count for the user and word
+        count_to_add = 5
+        self.sql_statements.insert_user_has_word(user_id, word, count_to_add)
+
+        # Call get_count method
+        self._logger.debug('Calling get_count method')
+        retrieved_count = self.sql_statements.get_count(user_id, word)
+
+        # Verify that the retrieved count is correct
+        self.assertEqual(retrieved_count, count_to_add, 'Retrieved count does not match expected count')
+        self._logger.info('get_count method tested successfully')
 
 
 if __name__ == '__main__':
     unittest.main()
+
+    # Drop tables afterwards
+    sql.SqlStatements.drop_tables()
