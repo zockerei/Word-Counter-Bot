73f8dddf0f147dc9f6e539d150352600fdbf505e
zockerei <2paulengelhardt@hitler.rocks>
2024-10-15 10:53:23 +0200

    Optimized the execution of queries. Seperated get_connection and execution of queries into 2 methods. Better error handling. Connection gets closed safely now to the database.

--- src/sql.py
+++ src/sql.py
@@ -8,18 +8,27 @@ class SqlStatements:
     """
     Class containing SQL statements and methods for executing them.
     """
-    # logging setup
     _sql_logger = logging.getLogger('bot.sql')
     _sql_logger.info('Logging setup complete')
 
-    # sqlite connection
-    try:
-        _sql_logger.debug('Setting up sql connection')
-        _sqlite_connection = sqlite3.connect(DB_PATH)
-        cursor = _sqlite_connection.cursor()
-        _sql_logger.info('Setup of sql connection complete')
-    except sqlite3.Error as error:
-        _sql_logger.error(f'Connection to database failed: {error}')
+    @staticmethod
+    def _get_connection():
+        """
+        Establishes a connection to the SQLite database.
+
+        Returns:
+            sqlite3.Connection: A connection object to the SQLite database.
+
+        Raises:
+            sqlite3.Error: If the connection to the database fails.
+        """
+        try:
+            SqlStatements._sql_logger.debug('Setting up sql connection')
+            connection = sqlite3.connect(DB_PATH)
+            return connection
+        except sqlite3.Error as error:
+            SqlStatements._sql_logger.error(f'Connection to database failed: {error}')
+            raise
 
     @staticmethod
     def _execute_query(
@@ -32,105 +41,102 @@ class SqlStatements:
         """
         Execute a SQL query and return the result.
 
-        Parameters:
+        Args:
             query (str): The SQL query to execute.
-            success_message (str, optional): Success message to log. Defaults to 'Success'.
-            error_message (str, optional): Error message to log. Defaults to 'Error'.
-            params (Dict[str, Any], optional): Parameters for the query as a dictionary of parameter names
-                and values. Defaults to None.
-            fetch_one (bool, optional): Whether to fetch only one result. Defaults to False.
+            success_message (str): Message to log on successful execution.
+            error_message (str): Message to log on execution error.
+            params (Optional[Dict[str, Any]]): Parameters for the SQL query.
+            fetch_one (bool): If True, fetch only one result.
 
         Returns:
-            Optional[List[Tuple]]: A list of tuples containing the result of the query, or None if an error occurred.
+            Optional[List[Tuple]]: The query results or None if an error occurred.
         """
+        connection = None
+        cursor = None
         try:
-            with SqlStatements._sqlite_connection:
-                if params:
-                    SqlStatements.cursor.execute(query, params)
-                else:
-                    SqlStatements.cursor.execute(query)
+            connection = SqlStatements._get_connection()
+            cursor = connection.cursor()
+            
+            if params:
+                cursor.execute(query, params)
+            else:
+                cursor.execute(query)
 
-                if fetch_one:
-                    result = SqlStatements.cursor.fetchone()
-                else:
-                    result = SqlStatements.cursor.fetchall()
-
-                SqlStatements._sql_logger.debug(success_message)
-                return result
+            result = cursor.fetchone() if fetch_one else cursor.fetchall()
+            connection.commit()
+            SqlStatements._sql_logger.debug(success_message)
+            return result
 
         except sqlite3.Error as error:
             SqlStatements._sql_logger.error(f'{error_message}: {error}')
             return None
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
 
     @staticmethod
     def drop_tables():
         """
         Drop all tables from the database.
-        Only for unit_tests
+        Only for unit_tests.
         """
         SqlStatements._sql_logger.debug('Dropping all tables')
-        SqlStatements.cursor.execute('drop table if exists user')
-        SqlStatements.cursor.execute('drop table if exists user_has_word')
-        SqlStatements.cursor.execute('drop table if exists word')
-        SqlStatements._sql_logger.info('Dropped all tables')
+        queries = [
+            'drop table if exists user',
+            'drop table if exists user_has_word',
+            'drop table if exists word'
+        ]
+        for query in queries:
+            SqlStatements._execute_query(query, 'Dropped table', 'Failed to drop table')
 
     @staticmethod
     def create_tables():
         """
         Create tables for the database.
         """
-        # Create user table
-        user_table_script = """
-            create table if not exists user (
-                id integer primary key,
-                permission text check (permission in ('admin', 'user'))
-            );"""
-        SqlStatements._execute_query(
-            user_table_script,
-            'User table created',
-            'Failed to create user table'
-        )
-        # Create word table
-        word_table_script = """
-            create table if not exists word (
-                name text primary key
-            );"""
-        SqlStatements._execute_query(
-            word_table_script,
-            'Word table created',
-            'Failed to create word table'
-        )
-        # Create user_has_word table
-        user_has_word_table_script = """
-            create table if not exists user_has_word (
-                user_id integer,
-                word_name varchar(45),
-                count integer,
-                foreign key (user_id) references user (id),
-                foreign key (word_name) references word (name)
-            );"""
-        SqlStatements._execute_query(
-            user_has_word_table_script,
-            'User_has_word table created',
-            'Failed to create user_has_word table'
-        )
+        table_scripts = {
+            "user": """
+                create table if not exists user (
+                    id integer primary key,
+                    permission text check (permission in ('admin', 'user'))
+                );""",
+            "word": """
+                create table if not exists word (
+                    name text primary key
+                );""",
+            "user_has_word": """
+                create table if not exists user_has_word (
+                    user_id integer,
+                    word_name varchar(45),
+                    count integer,
+                    foreign key (user_id) references user (id),
+                    foreign key (word_name) references word (name)
+                );"""
+        }
+        for name, script in table_scripts.items():
+            SqlStatements._execute_query(
+                script,
+                f'{name.capitalize()} table created',
+                f'Failed to create {name} table'
+            )
 
     @staticmethod
     def add_words(*words):
         """
         Add words to the database if they don't exist.
 
-        Parameters:
-            *words (str): Words to add to the database.
+        Args:
+            *words: Variable length argument list of words to add.
         """
         SqlStatements._sql_logger.debug('Inserting words into the database')
-
+        query = "insert or ignore into word values (:word);"
         for word in set(words):
-            query = "insert or ignore into word values (:word);"
             SqlStatements._execute_query(
                 query,
                 f'Word: {word} successfully inserted',
-                f'Failed to insert word: {word}',
+                f'Error inserting word: {word}',
                 {'word': word}
             )
 
@@ -139,27 +145,25 @@ class SqlStatements:
         """
         Add user IDs to the database if they don't exist.
 
-        Parameters:
-            *user_ids (int): User IDs to add to the database.
+        Args:
+            *user_ids: Variable length argument list of user IDs to add.
         """
         SqlStatements._sql_logger.debug('Inserting all users to the database')
-
         query = "insert or ignore into user values (:user_id, 'user')"
         for user_id in set(user_ids):
             SqlStatements._execute_query(
                 query,
                 f'User {user_id} inserted into the database',
-                f'Error inserting user {user_id} to the database',
+                f'Error inserting user ID: {user_id}',
                 {'user_id': user_id}
             )
-        SqlStatements._sql_logger.debug('All members inserted into the database')
 
     @staticmethod
     def add_admins(*user_ids: int) -> None:
         """
         Add admin permission to the specific user IDs.
 
-        Parameters:
+        Args:
             *user_ids (int): The IDs of the users to be granted admin permission.
         """
         SqlStatements._sql_logger.debug('Adding admins')
@@ -183,7 +187,7 @@ class SqlStatements:
         """
         Insert a new user_has_word record.
 
-        Parameters:
+        Args:
             user_id (int): The ID of the user.
             word (str): The word to be associated with the user.
             count (int): The count of the word for the user.
@@ -206,7 +210,7 @@ class SqlStatements:
         """
         Remove a word from the database.
 
-        Parameters:
+        Args:
             word (str): The word to be removed.
         """
         query = "delete from word where name = :word;"
@@ -223,12 +227,12 @@ class SqlStatements:
         """
         Get the count for a specific user ID and word.
 
-        Parameters:
+        Args:
             user_id (int): The ID of the user.
             word (str): The word to get the count for.
 
         Returns:
-            Optional[int]: The count of the word for the user, or None if an error occurred.
+            int | None: The count of the word for the user, or None if an error occurred.
         """
         SqlStatements._sql_logger.debug(f'Get count for user: {user_id} with word: {word}')
 
@@ -289,7 +293,7 @@ class SqlStatements:
         """
         Get user with the highest count for a specific word.
 
-        Parameters:
+        Args:
             word (str): The word for which to find the user with the highest count.
 
         Returns:
@@ -335,48 +339,42 @@ class SqlStatements:
         """
         Update user count for a specific word.
 
-        Parameters:
+        Args:
             user_id (int): The ID of the user.
             word (str): The word to update the count for.
-            count (int): The count to update.
+            count (int): The count to add to the existing count.
         """
         SqlStatements._sql_logger.debug('Updating count')
-        if SqlStatements.check_user_has_word(user_id, word):
-            current_count = SqlStatements.get_count(user_id, word)
-
-            query = """update user_has_word
-                        set count = :count
-                        where user_id = :user_id
-                        and word_name = :word;"""
-
-            SqlStatements._execute_query(
-                query,
-                f'Updated count of user: {current_count + count}',
-                params={'count': current_count + count, 'user_id': user_id, 'word': word}
-            )
-        else:
-            SqlStatements.add_user_has_word(user_id, word, count)
+        query = """
+            insert into user_has_word (user_id, word_name, count)
+            values (:user_id, :word, :count)
+            on conflict(user_id, word_name) do update set count = count + :count;
+        """
+        SqlStatements._execute_query(
+            query,
+            f'Updated count for user: {user_id} with word: {word}',
+            f'Error updating count for user: {user_id} with word: {word}',
+            {'user_id': user_id, 'word': word, 'count': count}
+        )
 
     @staticmethod
-    def check_user_has_word(user_id: int, word: str) -> tuple:
+    def check_user_has_word(user_id: int, word: str) -> bool:
         """
         Check if a user has an association with a specific word.
 
-        Parameters:
+        Args:
             user_id (int): The ID of the user.
-            word (str): The word to check association with.
+            word (str): The word to check for association.
 
         Returns:
             bool: True if the user has an association with the word, False otherwise.
         """
         SqlStatements._sql_logger.debug('Check if user has association with word')
-
         query = """select exists (
                      select 1 from user_has_word
                      where user_id = :user_id
                      and word_name = :word
                      )"""
-
         exists = SqlStatements._execute_query(
             query,
             f'User: {user_id} has association with the word: {word}',
@@ -384,34 +382,27 @@ class SqlStatements:
             {'user_id': user_id, 'word': word},
             fetch_one=True
         )
-        return exists[0]
+        return bool(exists[0])
 
     @staticmethod
     def check_user_is_admin(user_id: int) -> bool:
         """
         Check if a user has admin privileges.
 
-        Parameters:
-            user_id (int): The ID of the user.
+        Args:
+            user_id (int): The ID of the user to check.
 
         Returns:
             bool: True if the user has admin privileges, False otherwise.
         """
         SqlStatements._sql_logger.debug(f'Check if user has admin privileges')
-
         query = """select permission from user
                 where id = :user_id"""
-
         permission = SqlStatements._execute_query(
             query,
             f'Success in check_user_is_admin',
             f'Error in check_user_is_admin',
             {'user_id': user_id},
-            True
-        )[0]
-        if permission == 'admin':
-            SqlStatements._sql_logger.debug(f'User {user_id} has admin privileges')
-            return True
-        else:
-            SqlStatements._sql_logger.debug(f'User {user_id} has no admin privileges')
-            return False
+            fetch_one=True
+        )
+        return permission[0] == 'admin'
